ff75b73ff93f3a5100d5ee12f2cf87640ce6e9be
Cristian <galviscristian538@gmail.com>
2025-03-16 18:21:02 -0500

    agregando crear productos y usuarios

--- app.js
+++ app.js
@@ -1,40 +1,39 @@
+
+
 import express from "express";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 import cors from "cors";
 import facturas from "./routes/facturar.js";
-import clientes from "./routes/productosr.js";
-import servicios from "./routes/usuariosr.js";
+import usuarios from "./routes/usuariosr.js"; 
+import servicios from "./routes/productosr.js"
 
 
 dotenv.config();
 
+
 const app = express();
 
-app.use(cors());
 
-app.use(express.json());
-app.use(express.static("public"));
+app.use(express.json()); 
+app.use(cors()); 
+app.use(express.static("public"))
 
+mongoose
+    .connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log("✅ Conectado a MongoDB"))
+    .catch((error) => console.error("❌ Error al conectar a MongoDB:", error));
 
-app.use(cors());
+// Rutas
+app.use("/api/facturas", facturas);
+app.use("/api/usuarios", usuarios);       
+app.use("/api/servicios",servicios)
 
-
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => console.log('Base de datos conectada'))
-    .catch((error) => console.log('Error en la conexión a la base de datos:', error));
-
-
-app.use("/api/facturas", facturas)
-app.use("/api/clientes", clientes)
-app.use("/api/servicios", servicios)
-
-
+// Puerto del servidor
 const PORT = process.env.PORT || 3200;
 app.listen(PORT, () => {
-    console.log(`servidor corriendo en el puerto HTTP://LOCALHOST:${PORT}`);
+    console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
 });
-
